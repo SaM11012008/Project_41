72e3028a9185d277acf7c2eb6afd1416422d1cd5
SamTheGreat <74596262+SaM11012008@users.noreply.github.com>
2021-05-08 12:23:20 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,53 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        // create the property for the score and intialize with zero
+        this.score = 0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    static updateScore(score) {
+        database.ref('/').update({
+            Score:score
+        })
+    }
+
+
+    getScore(){
+        database.ref('Score').on("Value",(data)=>{
+            this.score = data.val()
+        })
+    }
+
+    
+}
